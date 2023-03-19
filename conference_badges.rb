d3fc124e0136f84e0c7c9a3f3618f44c5dc47e01
Hannan <najma.mahat@student.moringaschool.com>
2023-03-19 07:47:11 +0300

    Completed lab on badges and schedules

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+def badge_maker name
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator arr
+    arr.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms arr
+    arr.map {|name, index| "Hello, #{name}! You'll be assigned to room #{arr.index(name) + 1}!"}
+end
+
+def printer arr
+    batch_badge_creator(arr).each {|guest| puts guest}
+    assign_rooms(arr).each {|room_msg| puts room_msg}
+end
